011a2e288ac1daff61621682c59881adc490fa90
asfalfa <glkin9@gmail.com>
2024-08-17 16:17:31 +0200

    Fix lint issues in components

--- app.js
+++ app.js
@@ -6,4 +6,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.listen(PORT)
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log('server started on port 5000')
+})
